b3089cf19eb639abb19dcada9ed9e7ba06f711a8
Nathan <nathanraysheryak@gmail.com>
2021-04-12 07:49:26 -0400

    work on side nav styling

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,26 +1,27 @@
 <template>
-  <div class="border">
+  <div class="border side-nav">
 
-    <router-link class="border" to="/friends">
-      <font-awesome-icon icon="user-friends" /> Friends
+    <router-link class="side-nav-item" active-class="active" to="/friends">
+      <font-awesome-icon icon="user-friends" class="side-nav-icon" /> Friends
     </router-link> 
     
     <br/><br/>
 
-    <router-link class="border" to="/settings">
-      <font-awesome-icon icon="cog" /> Settings
+    <router-link class="side-nav-item" active-class="active" to="/settings">
+      <font-awesome-icon icon="cog" class="side-nav-icon" /> 
+      <div>Settings</div>
     </router-link>
 
     <br/><br/>
     
-    <router-link class="border" to="/chat">
-      <font-awesome-icon icon="comments" /> Chat
+    <router-link class="side-nav-item" active-class="active" to="/chat">
+      <font-awesome-icon icon="comments" class="side-nav-icon" /> Chat
     </router-link>
 
     <br/><br/>
     
-    <router-link class="border" to="/">
-      <font-awesome-icon icon="sign-out-alt" /> LogOut
+    <router-link class="side-nav-item" active-class="active" to="/">
+      <font-awesome-icon icon="sign-out-alt" class="side-nav-icon" /> LogOut
     </router-link>
   </div>
 </template>
